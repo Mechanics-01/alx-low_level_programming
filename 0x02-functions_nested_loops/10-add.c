7c8ee8196193f78c6dce49b064c1eb127ee2854a
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-18 13:59:53 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add twos integers and return the result
+ *
+ * Return: results of the addtion of the two integers
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
