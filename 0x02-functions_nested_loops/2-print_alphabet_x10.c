7d100307a7aafde3babceaf051be3e1ba14bcdb5
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-18 08:14:49 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints the alphabtes
+ * in lower case, followed by a new line 10 times
+ * Return: No return
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	_putchar(10);
+	}
+}
