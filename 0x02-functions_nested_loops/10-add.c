aa7c7c98f4f7b723c72920ab6d57bec7533f45a8
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-18 14:01:52 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * add - add twos integers and return the result
- *
+ * @a: first number argument
+ * @b: second number argument
  * Return: results of the addtion of the two integers
  */
 
