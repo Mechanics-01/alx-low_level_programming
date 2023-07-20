4b924800ec98b707c096d94ef5331e10e6ee0f5b
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-20 10:06:34 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - Prints numbers from 1 to 100 but for multiples
+ * of three print Fizz instead and for multiples of five
+ * print Buzz and for both FizzBuzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
