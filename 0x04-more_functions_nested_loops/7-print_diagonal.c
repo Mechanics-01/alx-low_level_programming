64af0aa0f656d8ff73977ce639362d1924858040
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-20 09:57:49 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a daigonal line on terminal
+ * @n: target variable
+ *
+ * Return: Nothing
+ */
+
+void print_diagonal(int n)
+{
+	int ch, i;
+
+	if (n > 0)
+	{
+		for (ch = 0; ch <= n; ch++)
+		{
+			for (i = 0; i < ch; i++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar(10);
+}
