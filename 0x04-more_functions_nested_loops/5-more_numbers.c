a386445215208a0902edd6d012220572abeb4ae4
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-20 09:40:06 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * most_numbers - Prints 10 times numbers from 0-14
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int c, ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c >= 10)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar(10);
+	}
+}
