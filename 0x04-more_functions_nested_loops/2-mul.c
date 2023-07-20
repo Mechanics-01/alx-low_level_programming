a0d7733577a949811a8104f8578a90fa5273dc9f
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-20 09:35:25 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int mult = a * b;
+
+	return (mult);
+}
