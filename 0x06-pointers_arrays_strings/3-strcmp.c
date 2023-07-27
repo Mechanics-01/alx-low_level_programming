d18660f55e7209bc5897378a9a1d0ec2482ee36f
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-27 01:38:44 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: target string
+ * @s2: target string
+ *
+ * Return: value of comparison (int)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1 == s2 && *s1 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	if (s1 != s2)
+		i = s1 - s2;
+	return (i);
+}
