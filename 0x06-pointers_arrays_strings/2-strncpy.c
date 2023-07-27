f283e8068923483df5d4d3bf83c691d1b84c6c54
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-07-27 01:19:42 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncpy - copies a string
+ * @dest: target string
+ * @src: target string to be copied
+ * @n: Required number of bytes to be copied
+ *
+ * Return: void
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int k = 0;
+	
+	while (src[k] != '\0' && k < n)
+	{
+		dest[k] = src[k];
+		k++;
+	}
+	dest[k] = '\0';
+	return (dest);
+}
