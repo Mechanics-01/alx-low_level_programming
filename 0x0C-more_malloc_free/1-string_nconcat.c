7fb5fa2e0bac1bb1a3884e342cda2de8c1127cf3
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-08-10 09:19:21 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = s2_len;
 
 	/* Memory allocation */
-	s = malloc(sizeof(char) * n + sl_len + 1);
+	s = malloc(sizeof(char) * n + s1_len + 1);
 	if (s == NULL)
 		return (NULL);
 
