b740d304d41f37b407537d6559c3a68a3178a1ff
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-08-16 10:43:12 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name: input string
@@ -9,5 +10,4 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL)
 		(*f)(name);
-	return;
 }
