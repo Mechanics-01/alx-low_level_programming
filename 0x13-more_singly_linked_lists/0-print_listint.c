5f6931e8a43e3b4ccc24c85906278ae80fa40412
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-08-29 10:25:01 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list.
- * @h: singly linked list.
- *
- * Return: number of nodes as elements.
- */
-
+  * print_listint - prints all elements of a listint_t list.
+  * @h: pointer to first element in a list.
+  *
+  * Return:  number of nodes.
+  */
 size_t print_listint(const listint_t *h)
 {
 	int n;
