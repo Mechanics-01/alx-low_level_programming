8bfd274a2903e2ea7bace9a1b2f42c71b3bdb7f5
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-09-02 05:00:40 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an
+ * to an unsigned int.
+ * @b: pointer to a string of 0 and 1 chars
+ * Return: the converted number or 0 or NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int Integer_n = 0;
+	int binary_base = 1;
+	int Length = 0;
+	int i;
+
+	if (!b)
+		return (0);
+
+	/* Find the length of the binary string*/
+	while (b[length] != '\0')
+		Length++;
+
+	/* Process the binary digit from left to right*/
+	for (i = Length - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			integer_n += binary_base;
+		if (b[Length] != '0' && b[Length] != '1')
+			return (0);
+		binary_base *= 2;
+	}
+	return (Integer_n);
+}
