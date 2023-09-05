1afda070d4dd3e2bc7b887cb3c806aa1eeb850cd
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-09-05 10:29:45 -0700

    alternative solution

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * error_file - checks for error in file during
+ * open operation.
+ * @file_from: original file
+ * @file_to: destination file
+ * @argv: argument vector
+ * Return: Void
  */
+
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -23,16 +24,18 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - check the code if correct
+ * @argc: number of arguments
+ * @argv: argument vecctors
+ * Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	int write_bytes;
+	ssize_t n_bytes;
+	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -44,17 +47,16 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	n_bytes = 1024;
+	while (n_bytes == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		n_bytes = read(file_from, buffer, 1024);
+		if (n_bytes == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		write_bytes = write(file_to, buffer, n_bytes);
+		if (write_bytes == -1)
 			error_file(0, -1, argv);
 	}
-
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
@@ -65,7 +67,7 @@ int main(int argc, char *argv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
