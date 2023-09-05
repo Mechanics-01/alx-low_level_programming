cbbca9c9581963a385a78468cf485b304da0b42a
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-09-05 04:35:24 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (0);
 
-	for (n_byte = 0; text_content[n_byte]; n_byte++)i
+	for (n_byte = 0; text_content[n_byte]; n_byte++)
 	{
 		;
 	}
