42fa59d3ce55d5b79dc37ef4c7ff83946ce279fd
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-09-05 04:11:20 -0700

    alternative solution

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,37 +2,26 @@
 
 /**
  * create_file - creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
- *
- * Return: 1 if it success. -1 if it fails.
+ * @filename: filename
+ * @text_content: content in the file
+ * Return: 1 if success and -1 if fails
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
-
-	if (!filename)
-		return (-1);
+	int fd, write_byte, read_byte;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
+		return (0);
+	write_byte = write(fd, text_content, sizeof(text_content));
+
+	if (write_byte == -1)
+		return (0);
+
+	if (filename == NULL)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
 	close(fd);
-
 	return (1);
 }
