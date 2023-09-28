952e6fed0cd6860a3d4465c6b58a7b7a0aa6df9d
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-09-28 06:19:45 -0700

    solved betty errors

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_rescursion - returns the value of x raised to y
+ * _pow_recursion - returns the value of x raised to y
  * @x: number to be raised
  * @y: power
  * Return: value of x to the power y
@@ -13,6 +13,6 @@ int _pow_recursion(int x, int y)
 		return (1);
 	else if (y < 0)
 		return (-1);
-	else 
+	else
 		return (x * _pow_recursion(x, y - 1));
 }
