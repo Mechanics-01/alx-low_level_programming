14d56d78c60aa847e12447806f2e22c9b40cc478
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-02 16:52:17 -0700

    added 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints program name, after which
+ * it prints new line
+ * @argc: argument number
+ * @argv: argument vector
+ * Return: integer
+ */
+
+int main(int argc  __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
