7e51e6474071f2d6626929345b6b9eadfecb27ed
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-04 17:01:29 +0100

    solved betty errors

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,35 +12,39 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, size1, size2;
 	char *str;
 
-	if (s1 == NULL)
-		s1[0] = '\0';
-	if (s2 == NULL)
-		s2[0] = '\0';
-
-	for (i = 0; s1[i] != '\0'; i++)
-		;
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			;
+	}
 	size1 = i;
-
-	for (j = 0; s2[j] != '\0'; j++)
-		;
+	if (s2 != NULL)
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+			;
+	}
 	size2 = j;
-
-	str = malloc(sizeof(char) * (size1 + size2 - 1));
-
+	str = malloc(sizeof(char) * (size1 + size2));
 	if (str == NULL)
 		return (NULL);
-
+	if (s1 == NULL || s2 == NULL)
+	{
+		str[0] = '\0';
+		return (str);
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		str[i] = s1[i];
 	}
+	if (s1 == NULL)
+		i = 0;
+
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		str[i] = s2[j];
 		i++;
 	}
 	str[i] = '\0';
-
 	return (str);
 }
 
