8bae2ac3d8d76889cd754aec7028d30c06cf3f31
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-05 00:16:21 +0100

    added files

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,29 +2,22 @@
 
 /**
  * str_concat - concatenates two strings
- * @s1: First string
- * @s2: Second string
- * Return: pointer to catenated string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: char pointer
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j, size1, size2;
+	unsigned int i = 0, j, size = 0;
 	char *str;
 
 	if (s1 != NULL)
-	{
 		for (i = 0; s1[i] != '\0'; i++)
-			;
-	}
-	size1 = i;
+			size++;
 	if (s2 != NULL)
-	{
-		for (j = 0; s2[j] != '\0'; j++)
-			;
-	}
-	size2 = j;
-	str = malloc(sizeof(char) * (size1 + size2 + 2));
+		for (i = 0; s2[i] != '\0'; i++)
+			size++;
+	str = malloc(sizeof(char) * (size + 1));
 	if (str == NULL)
 		return (NULL);
 	if (s1 == NULL && s2 == NULL)
@@ -32,19 +25,17 @@ char *str_concat(char *s1, char *s2)
 		str[0] = '\0';
 		return (str);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		str[i] = s1[i];
-	}
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			str[i] = s1[i];
 	if (s1 == NULL)
 		i = 0;
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		str[i] = s2[j];
-		i++;
-	}
-	str[i] = '\0';
+	if (s2 != NULL)
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			str[i] = s2[j];
+			i++;
+		}
+	str[size] = '\0';
 	return (str);
 }
-
