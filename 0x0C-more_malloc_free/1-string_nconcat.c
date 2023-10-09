739cbb6d9a3749180cb7f9bbea36dc3615a954f8
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-09 18:42:33 +0100

    added 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,18 +11,21 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int i, j, size1, size2;
+	unsigned int i, j, size1 = 0, size2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
+
+	while (s1[size1] != '\0')
 		size1++;
 
-	for (i = 0; s2[i] != '\0'; i++)
-		size2++;
+	while (s2[size2] != '\0')
+		size2++
+
 	if (n >= size2)
 		n = size2;
 
@@ -31,15 +34,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (str == NULL)
 		return (NULL);
 
-	for (j = 0; j < size1; j++)
-		str[j] = s1[j];
+	for (i = 0; i < size1; i++)
+		str[i] = s1[i];
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n; j++, i++)
 	{
-		str[size1] = s2[j];
-		size1++;
+		str[i] = s2[j];
 	}
-	str[size1] = '\0';
+	str[i] = '\0';
 
 	return (str);
 }
