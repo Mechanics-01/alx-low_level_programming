d33da1099e42ab16ebab580c1e2e48680cb8fe4d
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-10 19:50:18 +0100

    made changes in 2-print_god

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,18 +8,19 @@
 
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL)
-		printf("Name: NIL\n");
-	else
-		printf("Name: %s\n", d->name);
+	if (d != 0)
+	{
 
-	if (d->age == 0)
-		printf("Age: NIL\n");
-	else
+		if (d->name == NULL)
+			printf("Name: NIL\n");
+		else
+			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
 
-	if (d->owner == NULL)
-		printf("Owner: NIL\n");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("Owner: NIL\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
