be0bf6b69f9fcc5f8b006070d7e65865babffe2d
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-10-10 20:43:17 +0100

    added files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,5 +28,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *str_cpy(char *dest, char *ini);
-
+void free_dog(dog_t *d);
 #endif /*DOG_H*/
