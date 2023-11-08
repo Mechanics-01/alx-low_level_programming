f7ed48a48baae098a64ff3496497019c6082a55e
Mechanics-01 <sule.m1300343@st.futminna.edu.ng>
2023-11-08 11:47:48 +0100

    updated it

---
+++ simple_shell/shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdbool.h>
+
+void exe_comd(char *input, char *av[]);
+
+#endif/*SHELL_H*/
